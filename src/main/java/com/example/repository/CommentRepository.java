187915742fb85696081a9e71d125b4a6e543db7b
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-06-03 17:06:00 +0900

    コメントリポジトリを作成しました。 コメントリポジトリに記事IDから検索を行う機能を追加しました。

---
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -0,0 +1,59 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Comment;
+
+/**
+ * コメントテーブル(comments)を操作するリポジトリ.
+ * 
+ * @author momoyo kanie
+ *
+ */
+@Repository
+public class CommentRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	/**
+	 * コメントドメインを返すRow_Maper.
+	 */
+	private final static RowMapper<Comment> COMMENT_ROW_MAPPER = (rs, i) -> {
+		Comment comment = new Comment();
+		comment.setId(rs.getInt("id"));
+		comment.setName(rs.getString("name"));
+		comment.setContent(rs.getString("content"));
+		comment.setArticleId(rs.getInt("article_id"));
+		return comment;
+	};
+
+	
+	
+	/**
+	 * 記事IDからコメントを検索する.
+	 * コメントは新しいものが上に来るよう並び替える。
+	 * 
+	 * @param articleId 検索する記事ID
+	 * @return コメント一覧
+	 */
+	public List<Comment> findByArticleId(int articleId) {
+
+		String sql = "SELECT id, name, content, article_id"
+				+ " FROM comments"
+				+ " WHERE article_id=:articleId"
+				+ " ORDER BY id DESC;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("articleId", articleId);
+		List<Comment> commentList = template.query(sql, param, COMMENT_ROW_MAPPER);
+		
+		return commentList;
+	}
+
+}
